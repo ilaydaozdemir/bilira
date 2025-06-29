0f28e65c6ceda969c42f28c1be02ce820ebba8e8
ozdemirilayda <ozdemirilayda95@gmail.com>
2025-06-29 13:45:24 +0300

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CreateUser from "./pages/CreatUser.tsx";
 
-function App() {
+import { UserProvider } from "./context/UserContext";
+import UserCard from "./pages/UserCard.tsx";
+
+const App = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <CreateUser />
-    </div>
+    <UserProvider>
+      <div className="min-h-screen flex items-center justify-center bg-gray-100">
+        <Router>
+          <Routes>
+            <Route path="/" element={<CreateUser />} />
+            <Route path="/user" element={<UserCard />} />
+          </Routes>
+        </Router>
+      </div>
+    </UserProvider>
   );
-}
+};
 
 export default App;
